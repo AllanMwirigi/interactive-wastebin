9009e3a2d5a614a4f46630afa0ca886373d4a504
AllanMwirigi <allo.mwirigi@gmail.com>
2021-09-15 22:36:58 +0300

    pir sensor, configure virtualenv

--- mcu/main.py
+++ mcu/main.py
@@ -1,7 +1,24 @@
-import pygame
-pygame.mixer.init()
-file_path = "/home/allan/Music/going_home.wav"
-pygame.mixer.music.load(file_path)
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+from gpiozero import MotionSensor, LED
+#from time import sleep, time
+from signal import pause
+
+# gpiozero always uses BCM
+
+pir = MotionSensor(4)
+red = LED(19)
+
+# A function to detect motion
+def on_motion():
+        print('Motion detected!')
+        # red.off()
+
+# A function when no motion is detected
+def no_motion():
+        print('No motion detected')
+        # red.on()
+
+pir.when_motion = on_motion
+# pir.when_no_motion = no_motion
+
+pause() # The process sleeps until a signal is received. And then the signal handler is called.
+# investigate whether app terminates once first signal is received i.e. motion detected
